3fc347400cd736bdf364973440e507a7971297d6
Anthony Cera <ace_cera@yahoo.com>
2018-07-06 10:57:33 -0700

    changed the state of the ingredients object by changing the quantity of the salad.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger.js';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 2, 
+            salad: 1, 
             bacon: 4,
             cheese: 2,
             meat: 2
