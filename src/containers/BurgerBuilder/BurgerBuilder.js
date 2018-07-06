4617f53e340ab37f6d39bfaec9595fee4f01938d
Anthony Cera <ace_cera@yahoo.com>
2018-07-06 11:54:59 -0700

    changed state so that the burger would only have the bread Top and Bottom.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from '../../components/Burger/Burger.js';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1, 
-            bacon: 4,
-            cheese: 2,
-            meat: 2
+            salad: 0, 
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
     render () {
