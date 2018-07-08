3f75bc1ca63c2cd8253a8d1ac9e474f618939afc
Anthony Cera <ace_cera@yahoo.com>
2018-07-07 19:00:21 -0700

    changed name of label to ingredientLabel.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {ingredientLabel: 'Salad', type: 'salad'},
+    {ingredientLabel: 'Bacon', type: 'bacon'},
+    {ingredientLabel: 'Cheese', type: 'cheese'},
+    {ingredientLabel: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
-    <div></div>
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.ingredientLabel} ingredientLabel={ctrl.ingredientLabel} />
+        ))}
+    </div>
 );
 
 export default buildControls;
