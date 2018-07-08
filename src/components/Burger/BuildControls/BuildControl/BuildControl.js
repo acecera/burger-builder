dc0d624fdee2bbead6958f2e0f9c379d748b2c45
Anthony Cera <ace_cera@yahoo.com>
2018-07-07 19:27:38 -0700

    added the add ingredient prop to buildControl prop.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.ingredientLabel}>{props.ingredientLabel}</div>
         <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
   
 );
