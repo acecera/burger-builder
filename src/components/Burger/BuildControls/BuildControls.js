ba43bfd74d737ab98052d086bede93b217294b2d
Anthony Cera <ace_cera@yahoo.com>
2018-07-07 19:47:52 -0700

    added removeingredient handler and Order now button.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,12 +11,16 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
             key={ctrl.ingredientLabel} 
             ingredientLabel={ctrl.ingredientLabel} 
-            added={() => props.ingredientAdded(ctrl.type)} />
+            added={() => props.ingredientAdded(ctrl.type)} 
+            removed={() => props.ingredientRemoved(ctrl.type)} 
+            disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
