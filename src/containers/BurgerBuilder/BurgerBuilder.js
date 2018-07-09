e456200e728f9042b33fdcfd346bf0d22ed8b334
Anthony Cera <ace_cera@yahoo.com>
2018-07-09 09:56:40 -0700

    added logic to update the order amount to the remove and add ingredient handler.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+                .map(igKey => {
+                    return ingredients[igKey]; 
+                })
+                .reduce((sum, el) => {
+                    return sum + el;
+                },0);
+            this.setState({purchasable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -32,6 +44,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -45,6 +58,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     render () {
         const disabledInfo = {
@@ -60,6 +74,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler} 
                     disabled={disabledInfo} 
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}/>
             </Aux>
         );
