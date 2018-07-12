6a2302303021ebfdde2915a235c8914720ecfc68
Anthony Cera <ace_cera@yahoo.com>
2018-07-12 12:07:14 -0700

    cleaned up the button listeners to make it more readable.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,13 @@ import classes from './BuildControl.js';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.ingredientLabel}>{props.ingredientLabel}</div>
-        <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
-        <button className={classes.More} onClick={props.added}>More</button>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>More</button>
     </div>
   
 );
