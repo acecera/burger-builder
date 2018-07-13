758f10f778dd6df4e1746f8fea1ddd49fefd2e40
Anthony Cera <ace_cera@yahoo.com>
2018-07-13 11:01:38 -0700

    added logic for purchase handler continue along with a path to the checkout page.c

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,29 +84,17 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Ace',
-                address: {
-                    street: "Street",
-                    zipCode: '91505',
-                    country: "USA"
-                },
-                email: 'test@yahoo.com'
-            },
-            deliveryMethod: 'Fastest'
+        alert('You continue!');
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-             .then(response => {
-                 this.setState({loading: false, purchasing: false});
-             })
-             .catch(error => {
-                 this.setState({loading: false, purchasing: false})
-             });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render () {
