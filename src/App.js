92306e8ef78a17b951023793b4b51ef083685187
Anthony Cera <ace_cera@yahoo.com>
2018-07-13 11:10:43 -0700

    deleted an empty line of code to save space.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/" component={BurgerBuilder} />
-
           </Switch>
         </Layout>
       </div>
