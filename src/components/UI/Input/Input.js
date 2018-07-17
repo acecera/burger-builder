5373fdccc63d5fd3f71890ec5378fb51f79d63bc
Anthony Cera <ace_cera@yahoo.com>
2018-07-16 17:38:26 -0700

    added the value and onChange listener to the input element of the contact data form.

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,7 +5,7 @@ const input = (props) => {
     let inputElement = null;
     const inputClasses = [classes.InputElement];
 
-    if(props.invalid && props.shouldValidate) {
+    if(props.invalid && props.shouldValidate && props.touched) {
         inputClasses.push(classes.Invalid);
     }
 
@@ -14,18 +14,20 @@ const input = (props) => {
             inputElement = <input 
                 className={inputClasses.join(' ')} 
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed} />;
             break;
         case ('textarea'):
             inputElement = <textarea 
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed} />;
             break;
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     value={props.value}
                     onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
@@ -35,16 +37,21 @@ const input = (props) => {
                     ))}
                 </select>
             );
+            break;
         default:
             inputElement = <input 
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
-                value={props.value}/>;
-    }   
-    <div className={classes.Input}>
-        <label className={classes.Label}>{props.label}</label>
-        {inputElement}
-    </div>
+                value={props.value}
+                onChange={props.changed} />;
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
 };
 
 export default input;
