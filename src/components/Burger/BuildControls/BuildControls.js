ccf435e57266901b904ebb35f92bc097047e9486
Anthony Cera <ace_cera@yahoo.com>
2018-07-16 18:06:52 -0700

    added the disabled listener to the BuildControl tag.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,16 +15,16 @@ const buildControls = (props) => (
         <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
-            key={ctrl.ingredientLabel} 
-            ingredientLabel={ctrl.ingredientLabel} 
-            added={() => props.ingredientAdded(ctrl.type)} 
-            removed={() => props.ingredientRemoved(ctrl.type)} 
-            disabled={props.disabled[ctrl.type]} />
+                key={ctrl.ingredientLabel} 
+                ingredientLabel={ctrl.ingredientLabel} 
+                added={() => props.ingredientAdded(ctrl.type)} 
+                removed={() => props.ingredientRemoved(ctrl.type)} 
+                disabled={props.disabled[ctrl.type]} />
         ))}
         <button 
-        className={classes.OrderButton}
-        disabled={!props.purchasable}
-        onClick={props.ordered}>ORDER NOW</button>
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW</button>
     </div>
 );
 
