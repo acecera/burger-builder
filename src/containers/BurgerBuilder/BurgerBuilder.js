e40f07b6874a132e06007f30b906e959b7b2d9ca
Anthony Cera <ace_cera@yahoo.com>
2018-07-16 18:17:09 -0700

    commented out the get request in the componentDidMount funciton.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,13 +28,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props);
-        axios.get('https://react-burger-builder-83d6f.firebaseio.com/ingredients.json')
-             .then(response => {
-                this.setState({ingredients: response.data});
-             })
-             .catch(error => {
-                 this.setState({error: true})
-             });
+        // axios.get('https://react-burger-builder-83d6f.firebaseio.com/ingredients.json')
+        //      .then(response => {
+        //         this.setState({ingredients: response.data});
+        //      })
+        //      .catch(error => {
+        //          this.setState({error: true})
+        //      });
     }
 
     updatePurchaseState (ingredients) {
