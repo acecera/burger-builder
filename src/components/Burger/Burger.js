0db782dc3e9524893835de7f71bc671636154db7
Anthony Cera <ace_cera@yahoo.com>
2018-07-16 18:05:40 -0700

    added a console.log for the props.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,7 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    console.log(props);
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -13,7 +14,7 @@ const burger = (props) => {
         return arr.concat(el)
     }, []);
     if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please add ingredients!</p>
+        transformedIngredients = <p>Please add ingredients!</p>;
     }
     return (
         <div className={classes.Burger}>
