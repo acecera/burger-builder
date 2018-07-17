a4511d0b104fbbe0efe854a6815737afc8f2e520
Anthony Cera <ace_cera@yahoo.com>
2018-07-17 15:40:00 -0700

    added cases for the getOrderStart, getOrderFail, getOrderSuccess.

---
+++ src/store/reducers/order.js
@@ -0,0 +1,58 @@
+import * as actions from '../actions/actionTypes';
+
+const initialState = {
+    orders: [],
+    loading: false,
+    purchased: false
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_INITIALIZE:
+            return {
+                ...state,
+                purchased: false
+            }
+        case actionTypes.PURCHASE_BURGER_START:
+            return {
+                ...state,
+                loading: true
+            };
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+        const newOrder = {
+            ...action.orderData,
+            id: action.orderId
+        }
+            return {
+                ...state,
+                loading: false,
+                purchased: true,
+                orders: state.orders.concat(newOrder),
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false,
+            };
+        case actionTypes.GET_ORDERS_START:
+            return {
+                ...state,
+                loading: true
+            };
+        case actionTypes.GET_ORDERS_SUCCESS:
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            };
+        case actionTypes.GET_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
