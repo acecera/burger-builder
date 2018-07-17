f9be143357bf88b6416e68a9a9f0943140149a1f
Anthony Cera <ace_cera@yahoo.com>
2018-07-16 17:32:00 -0700

    added burger variable to insert into my if statement.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -108,26 +108,26 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
         let orderSummary = null;
-        let burger = this.state.error ? <p>Ingredients can not be loaded! </p> : <Spinner />;
+        let burger = this.state.error ? <p>Ingredients can not be loaded!</p> : <Spinner />;
 
         if(this.state.ingredients) {
             burger =  (
                 <Aux>
-                    <Burger ingredients={this.state.ingredients}/>
+                    <Burger ingredients={this.state.ingredients} />
                     <BuildControls 
                         ingredientAdded={this.addIngredientHandler}
                         ingredientRemoved={this.removeIngredientHandler} 
                         disabled={disabledInfo} 
                         purchasable={this.state.purchasable}
                         ordered={this.purchaseHandler}
-                        price={this.state.totalPrice}/>
+                        price={this.state.totalPrice} />
                 </Aux>
             );
-                orderSummary = <OrderSummary 
-                    ingredients={this.state.ingredients} 
-                    price={this.state.totalPrice}
-                    purchaseCancelled={this.purchaseCancelHandler}
-                    purchaseContinue={this.purchaseContinueHandler} />;
+            orderSummary = <OrderSummary 
+                ingredients={this.state.ingredients} 
+                price={this.state.totalPrice}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinue={this.purchaseContinueHandler} />;
         }
         if (this.state.loading) {
             orderSummary = <Spinner />;
