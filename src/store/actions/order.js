949371ee03c2b0455c0d05b45c30b194441e55e2
Anthony Cera <ace_cera@yahoo.com>
2018-07-22 19:27:56 -0700

    corrected the syntax for the queryparams variable.

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = (orderData, token) => {
         dispatch(purchaseBurgerStart());
         axios.post('/orders.json?auth=' + token, orderData) 
             .then(response => {
-                console.log(response.data);
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData));
             })
             .catch(error => {
@@ -65,7 +64,7 @@ export const getOrdersStart = () => {
 export const getOrders = (token, userId) => {
     return dispatch => {
         dispatch(getOrdersStart());
-        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo=' + userId + '"';
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
         axios.get('/orders.json' + queryParams)
             .then(res => {
                  const fetchedOrders = [];
